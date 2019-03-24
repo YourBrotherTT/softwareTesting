48bc3bdc663999901d7cd868860c7c88cc7007b3
YourBrotherTT <39661906+YourBrotherTT@users.noreply.github.com>
2019-03-24 22:16:45 +0800

    Add files via upload

---
+++ src/st/Calculate.java
@@ -0,0 +1,20 @@
+package st;
+
+public class Calculate {
+	
+	public int add(int a, int b) {
+		return a+b ;
+	}
+	
+	public int substract(int a , int b ) {
+		return a - b ;
+	}
+	
+	public int divide (int a , int b ) {
+		return a/b;
+	}
+	
+	public int multiply(int a , int b ) {
+		return a*b;
+	}
+}
